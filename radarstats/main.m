4632dd509722e56734cf158702469b16144780ad
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2012-03-16 23:34:40 +0100

    Initial Commit

---
+++ radarstats/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  radarstats
+//
+//  Created by Nicolas Bouilleaud on 16/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
